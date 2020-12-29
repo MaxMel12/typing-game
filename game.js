f1777e48a505105ee9d19db198a59eb8005461a0
Max <maxmel7@gmail.com>
2020-12-29 00:49:06 -0600

    script

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
 var WebSocketClient = require('websocket').client;
 var client = new WebSocketClient();
 
-client
+//client
+
+num = 0
+increment = () => {
+    num = num+1 
+    document.getElementById("num").attribute = num    
+}
