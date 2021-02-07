a44bef1504c6b1a1fb5318a985a0710be0eb947f
maxmel12 <maxmel7@gmail.com>
2021-02-07 14:54:03 -0600

    ok now

--- screens/splash/splash.js
+++ screens/splash/splash.js
@@ -7,14 +7,14 @@ document.getElementById("join_btn").addEventListener("click",()=>join())
 
 const join = () => {
     const code = document.getElementById("game-code-inp").value
-    window.location = 'typing-game/join?code='+code
+    window.location = '/typing-game/join?code='+code
     //window.location = '/join'
 }
 
 const host = () => {
-    window.location = 'typing-game/host'
+    window.location = '/typing-game/host'
 }
 
 const solo = () => {
-    window.location = 'typing-game/solo'
+    window.location = '/typing-game/solo'
 }
